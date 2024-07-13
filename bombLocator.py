f72b920865d30cf872209298b1de7a68283509d2
atsbomb <48645562+atsbomb@users.noreply.github.com>
2024-07-13 22:21:28 +0800

    migrated functions and stats var to bombLib3

--- bombLocator.py
+++ bombLocator.py
@@ -1,48 +1,26 @@
 import maya.cmds as cmds
 import maya.mel as mel
+from bombLib3 import SceneState
 
-class BombLocator:
+class BombLocator(SceneState):
     def __init__(self):
+        super().__init__()
+        
         # CONSTANT
         self.warning_nothing_selected = 'Nothing is selected. Creating a locator at the origin instead.'
         self.bomb_locator_attribute_name = 'bombLocator'
         self.source_attribute_name = 'source'
 
         # CLASS VARIABLE
-        self.selection = cmds.ls(sl=1, fl=1) if cmds.ls(sl=1, fl=1) else 0
-        self.component_selection = self._isSelectionComponent()
-        self.active_range = self._getActiveRange()
+        self.active_range = self.getActiveRange()
         self.generated_locators = []
-
-    def _isSelectionComponent(self):
-        if self.selection:
-            for sel in self.selection:
-                if sel.find('[') != -1:
-                    return 1
-                else:
-                    pass
-            return 0
-
-    def _getActiveRange(self):
-        """ Get selected range if time slider range is selected. Otherwise set current time and +1 range. """
-        timeSlider = mel.eval('$tmp = $gPlayBackSlider')
-        range_selected = 1 if cmds.timeControl(timeSlider, q=1, rv=1) else 0
-
-        if range_selected:
-            active_start = cmds.timeControl(timeSlider, q=1, ra=1)[0]
-            active_end = cmds.timeControl(timeSlider, q=1, ra=1)[1]
-            return range(int(active_start), int(active_end))
-        else:
-            current_time = cmds.currentTime(q=1)
-            current_time_plus_one = cmds.currentTime(q=1) + 1
-            return range(int(current_time), int(current_time_plus_one))
 
     def _generateLocator(self, source):
         """ Actual locator generation.Adding meta data for later use. """
         loc = cmds.spaceLocator()[0]
 
         # same rotation order as source, skip if nothing is selected.
-        if source and self.component_selection == 0:
+        if source and self.isSelectionComponent() == 0:
             cmds.setAttr(loc + '.ro', cmds.getAttr(source + '.ro'))
         if source:
             # embed meta data
@@ -64,28 +42,21 @@ class BombLocator:
 
         return bomb_locators
 
-    def currentTimeState(func):
-        def wrapper(self):
-            current_time = cmds.currentTime(q=1)
-            func(self)
-            cmds.currentTime(current_time)
-        return wrapper
-
-    @currentTimeState
+    @SceneState.restoreCurrentTime
     def createLocator(self):
         # if nothing is selected, create a single locator at the origin and exit.
-        if not self.selection:
+        if not self.sels:
             cmds.warning(self.warning_nothing_selected)
             self._generateLocator(source = '')
             return 0
 
         # create a static locator if time range wasn't selected.
-        for sel in self.selection:
+        for sel in self.sels:
             loc = self._generateLocator(source = sel)
 
             # align with selection
             cmds.xform(loc, ws=1, t=cmds.xform(sel, q=1, ws=1, t=1))
-            if self.component_selection:
+            if self.isSelectionComponent():
                 pass
             else:
                 cmds.xform(loc, ws=1, ro=cmds.xform(sel, q=1, ws=1, ro=1))
